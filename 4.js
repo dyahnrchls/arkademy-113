c357069f8226d93ae795ec82ee1e45a84d2128dd
dyah <dyahnrchls@gmail.com>
2019-07-13 18:16:09 +0700

    modified

--- 4.js
+++ 4.js
@@ -41,12 +41,18 @@ function buyEgg(date, money){
 
 
 function isPrime(num){
-    for(let i=2; i<num; i++){
-        if(num % i == 0){
-            return false;
-        }
-    }
-  return num > 1;
+    if (num===1){
+        return false;
+      }else if(num === 2){
+          return true;
+      }else{
+          for(var x = 2; x < num; x++){
+              if(num % x === 0){
+              return false;
+              }
+          }
+          return true;
+      }
 }
 
 buyEgg(25, 50000);
